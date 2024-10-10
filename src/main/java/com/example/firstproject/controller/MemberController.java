a7b638601d836779aafdcd30ebd2faa878fa195b
pakms980319 <pakms980319@gmail.com>
2024-10-10 23:57:10 +0900

    H2 데이터베이스 콘솔 사용, 데이터베이스 쿼리 실습, 롬복으로 코드 리팩토링(코드 간소화, 로깅) (2)

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
@@ -20,11 +22,14 @@ public class MemberController {
 
     @PostMapping("/join")
     public String joinMember(MemberForm memberForm) {
-        System.out.println("memberForm: " + memberForm.toString());
+        // System.out.println("memberForm: " + memberForm.toString());
+        log.info(memberForm.toString());
         Member member = memberForm.toEntity();
-        System.out.println("Entity member: " + member.toString());
+        // System.out.println("Entity member: " + member.toString());
+        log.info(member.toString());
         Member savedMember = memberRepository.save(member);
-        System.out.println("Saved Entity member: " + savedMember.toString());
+        // System.out.println("Saved Entity member: " + savedMember.toString());
+        log.info(savedMember.toString());
 
         return "";
     }

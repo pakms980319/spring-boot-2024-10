6cdb4951a9a845e108c3be605c86073d1814204e
pakms980319 <pakms980319@gmail.com>
2024-10-13 21:11:53 +0900

    단일, 전체 데이터 조회 및 요청에 따른 자동 뷰 페이지 생성 실습

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,13 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -32,5 +37,19 @@ public class MemberController {
         log.info(savedMember.toString());
 
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        List<Member> memberList = (ArrayList<Member>)memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+        return "members/index";
     }
 }
